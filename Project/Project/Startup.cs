1336a5eff31ea0cfe816880f8751093e3c22a0b2
Kseniya <Kseniya.Patseyeva@itechart-group.com>
2019-11-19 13:09:43 +0300

    Created Client with Create-React-App

--- Project/Project/Startup.cs
+++ Project/Project/Startup.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace Project
 {
@@ -16,6 +13,8 @@ namespace Project
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "Client/build"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +27,18 @@ namespace Project
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseMvc();
+            app.UseSpa(spa =>
             {
-                endpoints.MapGet("/", async context =>
+                spa.Options.SourcePath = Path.Join(env.ContentRootPath, "client");
+
+                if (env.IsDevelopment())
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
     }
