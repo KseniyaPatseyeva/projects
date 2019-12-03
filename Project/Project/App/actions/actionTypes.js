81dd0eaf598bdb083fe32fb29870da808809d01b
Kseniya <Kseniya.Patseyeva@itechart-group.com>
2019-12-03 15:21:23 +0300

    Fix actionTypes import statements

--- Project/Project/App/actions/actionTypes.js
+++ Project/Project/App/actions/actionTypes.js
@@ -4,6 +4,5 @@ export const DECREMENT = 'DECREMENT';
 export const INIT = 'INIT';
 
 // List
-export const ADD_ITEM = 'ADD_ITEM';
-export const REMOVE_ITEM = 'REMOVE_ITEM';
-export const GET_ITEMS = 'GET_ITEMS';
+export const CAR_ARRIVED = 'CAR_ARRIVED';
+export const CAR_LEFT = 'CAR_LEFT';
