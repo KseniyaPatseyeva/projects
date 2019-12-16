7a64e431da2a66ea1775ad116b91b928c87fcab2
Kseniya <Kseniya.Patseyeva@itechart-group.com>
2019-12-16 09:57:36 +0300

    Created Models classes
    Car and User classes

---
+++ Project/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models
+{
+    class Car
+    {
+        public int Id { get; set; }
+        public string LicensePlate { get; set; }
+        public DateTime ArrivedTime { get; set; }
+        public DateTime LefTime { get; set; }
+    }
+}
