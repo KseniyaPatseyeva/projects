1c906c1b8d0157a6525805d1d43f6b3c51ecfbd8
LozovskiArtyom <57658314+LozovskiArtyom@users.noreply.github.com>
2019-12-18 17:52:05 +0300

    EF DB

---
+++ Parking/Parking/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string carNumber { get; set; }
+        public bool isIn { get; set; }
+    }
+}
---
+++ Parking/Parking/dbo.Cars.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Cars] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [carNumber] NVARCHAR (MAX) NULL,
+    [isIn]      BIT            NOT NULL DEFAULT 0 ,
+    CONSTRAINT [PK_Cars] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
