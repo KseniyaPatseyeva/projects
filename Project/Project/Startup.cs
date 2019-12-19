46974dbf158cc994dc0c310bcd8db200ccd0a923
Kseniya <Kseniya.Patseyeva@itechart-group.com>
2019-12-19 11:55:48 +0300

    Remove unused dependencies in project

--- Project/Project/Startup.cs
+++ Project/Project/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using AutoMapper;
 using Microsoft.AspNetCore.Http;
 
 namespace Project
