e47a77505be95439eabc452e3a6d8d5348e95cf8
Kseniya <Kseniya.Patseyeva@itechart-group.com>
2020-01-13 12:57:20 +0300

    Separate model files

---
+++ Project/Models/DataRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{
+    public struct DataRecord
+    {
+        public DataRecord(string key, int count)
+        {
+            Key = key;
+            Count = count;
+        }
+
+        public string Key { get; set; }
+
+        public int Count { get; set; }
+    }
+}
--- Project/Models/StatData.cs
+++ Project/Models/StatData.cs
@@ -2,19 +2,6 @@
 
 namespace Models
 {
-    public struct DataRecord
-    {
-        public DataRecord(string key, int count)
-        {
-            Key = key;
-            Count = count;
-        }
-
-        public string Key { get; set; }
-
-        public int Count { get; set; }
-    }
-
     public class StatData
     {
         public StatData(IEnumerable<DataRecord> records, string label)
